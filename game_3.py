2a61607bc639393348044e93d5211631dd73e035
Vladimir Khromov <vlad2aria@mail.ru>
2024-02-06 00:01:49 +0300

    game 3

--- game_3.py
+++ game_3.py
@@ -1,4 +1,4 @@
-from game_3_lib import get_word
+from game_3_lib import get_word, rus_letters
 
 GAME_NAME = "Виселица"
 LEVELS = {"Легко": 0,
@@ -15,31 +15,46 @@ def update_personal_world(l: list, secret: str, quess: str) -> list:
     return l
 
 
+def check_letter(letter: str):
+    if len(letter) == 1 and letter in rus_letters:
+        return True
+    return False
 
-
-
-
-
-
+def get_attempt(level):
+    if level in range(0,len(LEVELS)):
+        return 10 - level*2
+    return 5
 
 def game(level):
+    # Получаем слово
     secret_word = get_word(level)
+    # Создаем табло для букв
     person_alpha = ["_" for _ in range(len(secret_word))]
+    # Даем количество попыток
+    attempt = get_attempt(level)
 
-    while True:
+    while person_alpha != list(secret_word):
         print(
             f"Загаданное слово {' '.join(person_alpha)}, {len(secret_word)} {'буквы' if len(secret_word) < 5 else 'букв'}")
-        guess = input("Введите букву:")
+        print(f' У вас осталось {attempt} попыток')
+        guess = input("Введите букву:").lower()
 
-        if guess in secret_word:
+        if not check_letter(guess):
+            print('Неверно, попробуйте еще раз')
+        elif guess in secret_word:
             print("Есть такая буква!")
             person_alpha = update_personal_world(person_alpha, secret_word, guess)
         else:
-            print("Такой буквы нет!")
+            attempt -= 1
+            if attempt == 0:
+                print("К сожалению вы проиграли!")
+                print(f'Правильное слобо было --{secret_word}--')
+                break
+            print(f"Такой буквы нет! Минус попытка")
 
-        # Проверка что все буквы угаданы
-        # Проверка что буква одна вводится пользователем
         # Добавить попытки
+    else:
+        print(f'Поздравляем! Вы угадали --{secret_word}-- и победили!')
 
 
 def game_start() -> None:
